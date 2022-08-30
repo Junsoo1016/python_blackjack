fa7fd0e21086b90b41e5e82bffec6f7b9770ae2f
Jun <junsoopark92@gmail.com>
2022-08-30 14:43:36 -0400

    game loop

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,6 @@
 import random
-
+playerIn = True
+dealerIn = True
 # deck of cards / player dealer hand
 deck = [2, 3, 4, 5 ,6, 7, 8, 9, 10, 2, 3, 4, 5 ,6, 7, 8, 9, 10, 2, 3, 4, 5 ,6, 7, 8, 9, 10, 2, 3, 4, 5 ,6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A']
 player_hand = []
@@ -38,3 +39,37 @@ for _ in range(2):
     dealCard(dealer_hand)
     dealCard(player_hand)
 
+while playerIn or dealerIn:
+    print(f"Dealer had {reavealDealerHand()} and X")
+    print(f"You have {player_hand} for a total of {total(player_hand)}")
+    if playerIn:
+        stayOrHit = int(input("1: Stay\n2: Hit\n"))
+    if total(dealer_hand) > 16:
+        dealerIn = False
+    else:
+        dealCard(dealer_hand)
+    if stayOrHit == 1:
+        playerIn = False
+    else:
+        dealCard(player_hand)
+    if total(player_hand) >= 21:
+        break
+    elif total(dealer_hand) >= 21:
+        break
+if total(player_hand) == 21:
+    print(f"\nYou have {player_hand} for a total of 21 and the dealer has {dealer_hand} for a total of {total(dealer_hand)}")
+    print("Blackjack! You win!")
+elif total(dealer_hand) == 21:
+    print(f"\nYou have {player_hand} for a total of 21 and the dealer has {dealer_hand} for a total of {total(dealer_hand)}")
+    print("Blackjack! Dealer wins!")
+elif total(player_hand) > 21:
+    print(f"\nYou have {player_hand} for a total of 21 and the dealer has {dealer_hand} for a total of {total(dealer_hand)}")
+    print("You bust! Dealer wins")
+elif total(dealer_hand) > 21:
+    print(f"\nYou have {player_hand} for a total of 21 and the dealer has {dealer_hand} for a total of {total(dealer_hand)}")
+    print("Dealer busts! You win!")
+elif 21 - total(dealer_hand) < 21 - total(player_hand):
+    print(f"\nYou have {player_hand} for a total of 21 and the dealer has {dealer_hand} for a total of {total(dealer_hand)}")
+elif 21 - total(dealer_hand) > 21 - total(player_hand):
+    print(f"\nYou have {player_hand} for a total of 21 and the dealer has {dealer_hand} for a total of {total(dealer_hand)}")
+    print("You win!")
